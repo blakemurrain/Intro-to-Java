89b18201732be5243c11510d786f8880f6bfe9d2
blakemurrain <84035472+blakemurrain@users.noreply.github.com>
2021-05-11 21:47:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var promptername = prompt("What is your name?");
+var promptercolor = prompt("What is your favorite color?");
+
+var hello1   = "Hello my name is " +promptername;
+var hello2 = "and my favorite color is "+promptercolor;
+
+document.querySelector(".hello").innerHTML = hello1 + hello2;
+
+
+
+
+
+
